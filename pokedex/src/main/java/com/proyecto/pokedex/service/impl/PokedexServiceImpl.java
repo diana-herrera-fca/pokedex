a8f2d902b186969d5b73b96e68c0b15e10dbb98b
diana-herrera-fca <dianakarenherreracarrillo@gmail.com>
2025-03-13 07:46:34 -0600

    First commit pokemon api

--- pokedex/src/main/java/com/proyecto/pokedex/service/impl/PokedexServiceImpl.java
+++ pokedex/src/main/java/com/proyecto/pokedex/service/impl/PokedexServiceImpl.java
@@ -13,16 +13,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedWriter;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PokedexServiceImpl implements PokedexService {
-
     RestTemplate restTemplate = new RestTemplate();
-    @Override
     @Cacheable(value = "pokemonCache", key = "#limit - #offset")
     public Page getAllPokemon(int limit, int offset) {
         final String uri = "https://pokeapi.co/api/v2/pokemon?limit="+limit+"&offset="+offset;
@@ -51,7 +47,7 @@ public class PokedexServiceImpl implements PokedexService {
             result.setDescription(getPokemonDescription(result.getId()));
             return result;
         }catch (HttpClientErrorException.NotFound e){
-            System.out.println("Not FOUND!!!");
+            System.err.println("Error fetching data: " + e.getMessage());
             return new PokemonDetailResponse();
         }
     }
@@ -67,46 +63,36 @@ public class PokedexServiceImpl implements PokedexService {
             if (speciesJson == null || !speciesJson.has("evolution_chain")) {
                 throw new RuntimeException("Evolution chain URL not found for " + pokemonName);
             }
-
             String evolutionChainUrl = speciesJson.get("evolution_chain").get("url").asText();
-
             ResponseEntity<JsonNode> evolutionResponse = restTemplate.getForEntity(evolutionChainUrl, JsonNode.class);
             JsonNode evolutionJson = evolutionResponse.getBody();
-
             JsonNode current = evolutionJson.get("chain");
-
             while (current != null) {
                 evolutions.add(getPokemonGeneralInformation(current.get("species").get("name").asText()));
                 current = current.get("evolves_to").size() > 0 ? current.get("evolves_to").get(0) : null;
             }
-
         } catch (Exception e) {
             System.err.println("Error fetching evolution data: " + e.getMessage());
         }
-
         return evolutions;
     }
+    @Cacheable(value = "pokemonCache", key = "#id-description")
     public String getPokemonDescription(int id) {
         try {
             String url = "https://pokeapi.co/api/v2/characteristic/" + id + "/";
             ResponseEntity<JsonNode> response = restTemplate.getForEntity(url, JsonNode.class);
             JsonNode jsonResponse = response.getBody();
-
             if (jsonResponse == null || !jsonResponse.has("descriptions")) {
                 throw new RuntimeException("No descriptions found for ID: " + id);
             }
-
             for (JsonNode descNode : jsonResponse.get("descriptions")) {
                 if ("en".equals(descNode.get("language").get("name").asText())) {
                    return (descNode.get("description").asText());
                 }
             }
-
         } catch (Exception e) {
-            System.err.println("Error fetching Pokémon characteristic: " + e.getMessage());
-            return "No description available";
+            System.err.println("Error fetching characteristic: " + e.getMessage());
         }
-
-        return "";
+        return "No description available";
     }
 }
